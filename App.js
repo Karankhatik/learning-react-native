a21bacdf89d5942eaff748289a9afd28bf62db6c
Karankhatik <100562135+Karankhatik@users.noreply.github.com>
2023-02-11 19:15:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      Hello World
+      <Text> Hello World </Text>
     </View>
   );
 }
